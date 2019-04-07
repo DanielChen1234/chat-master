e718e095da865d974202e9cec2132f69589a1428
DanielChen1234 <dchen8492@yahoo.com>
2019-04-07 11:51:19 -0400

    iphone x screen now fully utilized

--- App/index.js
+++ App/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, SafeAreaView } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
@@ -20,20 +20,19 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <View style={styles.container}>
+        <SafeAreaView style={styles.safeArea}>
            <ConnectedChatLog />
-         </View>
+         </SafeAreaView>
       </Provider>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
-    top: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 })
